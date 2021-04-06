dc4798b2a3a29840cbf1656ddb06c83ac3cdb0aa
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2021-04-06 19:18:33 +0200

    setup project and add style tabs section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookmarksComponent } from './dashboard/bookmarks/bookmarks.component';
+import { NotesComponent } from './dashboard/notes/notes.component';
+import { TodosComponent } from './dashboard/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"bookmarks", component:BookmarksComponent},
+  {path:"todos", component:TodosComponent},
+  {path:"notes", component:NotesComponent},
+  {path:"", component:BookmarksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './dashboard/bookmarks/bookmarks.component';
+import { TodosComponent } from './dashboard/todos/todos.component';
+import { NotesComponent } from './dashboard/notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    BookmarksComponent,
+    TodosComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
