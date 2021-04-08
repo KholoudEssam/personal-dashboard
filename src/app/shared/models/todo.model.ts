a2c8126dea8ba73f83cd488e2cf1a5a5cbbcc475
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2021-04-08 12:36:59 +0200

    todo model updated!!

--- src/app/shared/models/todo.model.ts
+++ src/app/shared/models/todo.model.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export class Todo {
   id: string;
-  constructor(public content: string) {
+  constructor(public text: string, public completed: boolean) {
     this.id = uuidv4();
   }
 }
