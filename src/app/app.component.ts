ddef876965d924f6622d98384f6c2fca88efe9a2
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2021-04-20 17:42:05 +0200

    adding dynamic bg and current date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -33,7 +35,29 @@ import { RouterOutlet } from '@angular/router';
   ],
 })
 export class AppComponent {
+  bgImg =
+    'https://images.unsplash.com/photo-1615569082599-865be05af048?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920';
+  imgLoaded = true;
+  dateTime = new Observable<Date>();
+
+  ngOnInit(): void {
+    this.dateTime = timer(0, 1000).pipe(map(() => new Date()));
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     if (outlet.isActivated) return outlet.activatedRoute.snapshot.url;
+  }
+
+  async changeBg() {
+    this.imgLoaded = false;
+    const newBg = await fetch('https://source.unsplash.com/random/1920x1080', {
+      method: 'HEAD',
+    });
+
+    if (newBg.url === this.bgImg) return this.changeBg();
+    this.bgImg = newBg.url;
+  }
+  imageLoaded() {
+    this.imgLoaded = true;
   }
 }
